9f3e8bf15bbad2e9da224753d62b0554e4ae5e5f
Thomas Levine <_@thomaslevine.com>
2015-03-29 10:01:35 +0000

    -

--- R/video.R
+++ R/video.R
@@ -1,8 +1,9 @@
 library(scales)
 library(ggplot2)
 library(RColorBrewer)
-# colors <- paste0(brewer.pal(12, 'Set3'), '99')
-# names(colors)[1:length(levels(anycast.probe$dst_city))] <- levels(anycast.probe$dst_city)
+
+colors <- paste0(brewer.pal(12, 'Set3'), '99')
+names(colors)[1:length(levels(anycast.probe$dst_city))] <- levels(anycast.probe$dst_city)
 
 
 video <- function(anycast.probe) {
@@ -11,20 +12,23 @@ video <- function(anycast.probe) {
   for (start in starts) {
     png(sprintf('frames/%s.png', start), width = 800, height = 450)
     df <- subset(anycast.probe, timestamp >= start & timestamp < start + step)
-    print(frame(anycast.probe, df))
+    frame(anycast.probe, df)
     dev.off()
   }
 }
 
-frame <- function(df.full, df)
-  ggplot(df) +
-    aes(x = dist_theoretical_improvement, y = dist, size = rt, color = dst_city) +
-    ggtitle('Targetting 173.245.58.117 (anycast)') +
-    scale_y_continuous('Distance to chosen instance (km)',
-                       labels = comma, limits = range(df.full$dist)) +
-    scale_x_continuous('Distance farther than the closest instance (km)',
-                       labels = comma, limits = range(df.full$dist_theoretical_improvement)) +
-    scale_size_continuous('Min response time (ms)', labels = comma) +
-    annotate('text', 0.9 * max(df.full$dist_theoretical_improvement), 0.5 * max(df.full$dist), label = 'Indirect routes') +
-    annotate('text', 0.1 * max(df.full$dist_theoretical_improvement), 0.5 * max(df.full$dist), label = 'Direct routes') +
-    geom_point(alpha = 0.5)
+frame <- function(df.full, df) {
+  plot(dist ~ dist_theoretical_improvement, data = df.full, type = 'n',
+       main = 'Targetting 173.245.58.117 (anycast)',
+       axes = F,
+       xlab = 'Distance farther than the closest instance (km)',
+       ylab = 'Distance to chosen instance (km)',
+       sub = 'Each dot is a measurement. Bigger dots have higher latency.',
+       xlim = range(df.full$dist_theoretical_improvement),
+       ylim = range(df.full$dist))
+  points(df$dist ~ df$dist_theoretical_improvement,
+         pch = 21, lwd = 0, cex = sqrt(df$rt), bg = colors[df$dst_city])
+  text(x = c(0.9, 0.1) * max(df.full$dist_theoretical_improvement),
+       y = 0.5 * max(df.full$dist),
+       label = c('Indirect routes', 'Direct routes'))
+}
