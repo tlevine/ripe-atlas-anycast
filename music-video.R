d7b6fbab6b82ad4640eb54e27d8de43d497b0d1a
Thomas Levine <_@thomaslevine.com>
2015-03-29 02:04:41 +0000

    -

--- music-video.R
+++ music-video.R
@@ -9,6 +9,7 @@ anycast$as <- anycast$asn_v4
 anycast[anycast$asf == 6,'as'] <- anycast[anycast$asf == 6,'asn_v6']
 anycast$asn_v4 <- anycast$asn_v6 <- NULL
 anycast$af <- factor(anycast$af, levels = c(4, 6))
+anycast$dst_city <- factor(anycast$dst_city)
 
 anycast.probe <- ddply(anycast, 'prb_id', function(df) {
   df[order(df$rt)[1],]
@@ -17,7 +18,12 @@ m <- rlm(rt ~ dist, data = anycast.probe)
 
 frame <- function(df.full, low, high) {
   df <- subset(df.full, dist >= low & dist < high)
-  plot(rt ~ theoretical_distance, data = df)
+  plot(dist ~ dist_theoretical, data = df.full, type = 'n',
+       axes = F)
+  axis(1)
+  axis(2)
+  points(df$dist ~ df$dist_theoretical, pch = 21, bg = df$dst_city,
+         )
 }
 
 N.VERSES <- 7
@@ -25,7 +31,7 @@ N.PHRASES <- N.VERSES * 4
 
 dist.step <- round(max(anycast$dist) / N.PHRASES, -2)
 for (dist in seq(0, max(anycast$dist) + dist.step, dist.step)) {
-  png(paste0('frames/', dist, '.png'), width = 800, height = 450)
+  png(sprintf('frames/%08d.png', dist), width = 800, height = 450)
   frame(anycast.probe, dist, dist + dist.step)
   dev.off()
 }
