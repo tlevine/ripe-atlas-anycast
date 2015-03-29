11178c93055dcbb7fea370d742b76d65fae13ec0
Thomas Levine <_@thomaslevine.com>
2015-03-29 10:22:00 +0000

    -

--- R/music.R
+++ R/music.R
@@ -1,13 +1,25 @@
+library(devtools)
+unloadNamespace('devtools')
 library(ddr)
 data(roland)
 unloadNamespace('ddr')
 
-library(devtools)
-unloadNamespace('devtools')
-devtools::load_all('../../krounq')
+devtools::load_all('../krounq')
+#source('generate-data.R')
 
-LIGHT.THROUGH.FIBER <- 1.444 * 2 * (1000/299792)
 TEMPO <- 216 # multiple of 24, for easy division
+
+norm <- function(x) {
+  x / max(abs(x))
+}
+
+drumlike <- function(freq, duration) {
+  base <- 0.8 * sawtooth(freq, duration) ^ 3 + runif(duration, -.2, .2)
+  duration.left <- min(duration, round(SECOND/8))
+  base.left <- base[1:duration.left]
+  base.right <- silence(max(0, duration - duration.left))
+  c(base.left, base.right)
+}
 
 sample.instrument <- function(the.sample) {
   function(., duration) {
@@ -20,46 +32,43 @@ sample.instrument <- function(the.sample) {
   }
 }
 
-.norm <- function(x) {
-  x / max(abs(x))
+kick <- sample.instrument(norm(roland$BD1@left[(1:length(roland$BD1@left)) %% 2 == 0]))
+snare <- sample.instrument(norm(roland$SD0@left))
+hihat <- sample.instrument(norm(roland$HHO@left))
+rim <- sample.instrument(norm(roland$RIM@left))
+
+phrase <- function(key = 30, speed = 0, pickup = NULL, drums = TRUE,
+                   rhythm = c(1, 4)) {
+  base.duration <- 2 ^ (4 - floor(speed))
+
+  pounding <- sequence(durations = base.duration,
+                       instrument = if (drums) snare else rim,
+                       tempo = TEMPO, beats = 8)
+
+  f <- rep(key, length(rhythm))
+  if (!is.null(pickup))
+    f[floor(rhythm) %% 4 == 0] <- key + pickup
+  melody <- sequence(frequencies = P.n(f),
+                     starts = rhythm,
+                     durations = 0.5,
+                     instrument = drumlike,
+                     tempo = TEMPO,
+                     beats = 8)
+
+  rep(2.5 * pounding + melody, 2)
 }
-kick <- sample.instrument(.norm(roland$BD1@left[(1:length(roland$BD1@left)) %% 2 == 0]))
-snare <- sample.instrument(.norm(roland$SD0@left))
-hihat <- sample.instrument(.norm(roland$HHO@left))
-
-drumlike <- function(freq, duration) {
-  base <- 0.8 * sawtooth(freq, duration) ^ 3 + runif(duration, -.2, .2)
-  duration.left <- min(duration, round(SECOND/8))
-  base.left <- base[1:duration.left]
-  base.right <- silence(max(0, duration - duration.left))
-  c(base.left, base.right)
-}
 
 
-phrase <- function(df.full, df, base.pitch = 22) {
-  # Rhythm speed: Number of measurements
-  base.duration <- 2 ^ (4 - floor(nrow(df) / 20))
-  speed <- ceiling(nrow(df) / 70)
+RHYTHMS <- list(c(1, 2, 3, 4.5, 5, 6, 7, 8, 8.5),
+                c(1, 2, 3, 3 + 2/3, 4 + 1/3, 5, 6, 6.5, 7, 7.5, 8, 8.5),
+                c(1, 3, 5, 7), 1:8)
 
-  # syncopation <- number of probes
+p <- function(row)
+  phrase(key = 30, speed = row$Sepal.Length - 1,
+         pickup = scales$major[round(row$Sepal.Width - 1)],
+         drums = row$Petal.Length > 3,
+         rhythm = RHYTHMS[[row$rhythm]])
 
-  # Response time divided by speed of light through fiber
-  .selector <- order(anycast.probe$rt)[round(nrow(anycast.probe)/2)]
-  .rt.normalized <- df[.selector,'rt'] / (LIGHT.THROUGH.FIBER * df[.selector,'dist'])
-  pitch <- base.pitch + intervals$P1 + (.rt.normalized > 3) * intervals$P4
-  
-  a <- sequence(durations = base.duration, instrument = snare,
-                tempo = TEMPO, beats = 8)
-  b <- function(key, speed) {
-    starts <- c(1, 2, 3, 4.5, 5, 6, 7, 8, 8.5)
-    start.priorities <- c(1, 5, 7, 3, 8, 9, 4, 7, 2)
-    sequence(frequencies = P.n(key),
-             starts = starts,
-             durations = base.duration,
-             instrument = drumlike[start.priorities[1:speed]],
-             tempo = TEMPO,
-             beats = 8)
-  }
-
-  c(b(base.pitch, speed), b(pitch, speed)) + a * 3
-}
+iris$rhythm <- as.numeric(iris$Species)
+is <- order(iris$Petal.Width)[seq(1, 150, length.out = 12)]
+play(do.call(c,lapply(is, function(i) p(iris[i,]))))
