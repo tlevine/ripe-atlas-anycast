0f84bbd6c62572519f05f226ac6aa6ed068d29e1
Thomas Levine <_@thomaslevine.com>
2015-03-29 01:50:41 +0000

    -

--- music-video.R
+++ music-video.R
@@ -15,14 +15,17 @@ anycast.probe <- ddply(anycast, 'prb_id', function(df) {
 })
 m <- rlm(rt ~ dist, data = anycast.probe)
 
-frame <- function(low, high) {
+frame <- function(df.full, low, high) {
+  df <- subset(df.full, dist >= low & dist < high)
+  plot(rt ~ theoretical_distance, data = df)
 }
-
 
 N.VERSES <- 7
 N.PHRASES <- N.VERSES * 4
 
 dist.step <- round(max(anycast$dist) / N.PHRASES, -2)
 for (dist in seq(0, max(anycast$dist) + dist.step, dist.step)) {
+  png(paste0('frames/', dist, '.png'), width = 800, height = 450)
   frame(anycast.probe, dist, dist + dist.step)
+  dev.off()
 }
