e02ca2f165f40d66102d198bfc885a83c7285ae6
Thomas Levine <_@thomaslevine.com>
2015-03-29 10:13:44 +0000

    -

--- R/video.R
+++ R/video.R
@@ -10,7 +10,7 @@ video <- function(anycast.probe) {
   step <- 3600
   starts <- seq(min(anycast.probe$timestamp), max(anycast.probe$timestamp) + step, step)
   for (start in starts) {
-    png(sprintf('frames/%s.png', start), width = 800, height = 450)
+    png(sprintf('frames/%s.png', start), width = 1600, height = 900)
     df <- subset(anycast.probe, timestamp >= start & timestamp < start + step)
     frame(anycast.probe, df)
     dev.off()
@@ -22,7 +22,8 @@ frame <- function(df.full, df) {
   bg <- 'black'
   par(fg = fg, col = fg, col.axis = fg,
       col.lab = fg, col.main = fg, col.sub = fg,
-      cex.axis = 2,
+ #    cex.axis = 2,
+      family = 'Helvetica',
       las = 1, bg = bg)
 
   plot(dist ~ dist_theoretical_improvement, data = df.full, type = 'n',
@@ -31,13 +32,13 @@ frame <- function(df.full, df) {
        xlab = 'Distance farther than the closest instance (km)',
        ylab = 'Distance to chosen instance (km)',
        sub = 'Each dot is a measurement. Bigger dots have higher latency.',
-       xlim = 1.2 * range(df.full$dist_theoretical_improvement),
-       ylim = 1.2 * range(df.full$dist))
-  axis(1, at = seq(0, max(df.full$dist_theoretical_improvement), 2e3))
-  axis(2, at = seq(0, max(df.full$dist), 2e3))
+       xlim = c(0.8, 1.2) * range(df.full$dist_theoretical_improvement),
+       ylim = c(0.8, 1.2) * range(df.full$dist))
+  axis(1, at = seq(0, max(df.full$dist_theoretical_improvement), 2e2))
+  axis(2, at = seq(0, max(df.full$dist), 1e3))
   points(df$dist ~ df$dist_theoretical_improvement,
          pch = 21, lwd = 0, cex = sqrt(df$rt), bg = colors[df$dst_city])
-  text(x = c(0.9, 0.1) * max(df.full$dist_theoretical_improvement),
+  text(x = c(0.8, 0.1) * max(df.full$dist_theoretical_improvement),
        y = 0.2 * max(df.full$dist),
        label = c('Indirect routes', 'Direct routes'))
 }
